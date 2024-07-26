dc47597e357070fc8711cae2f9ffd96ac73e33d3
Zharif Aziz <zharifazizzulkarnain@gmail.com>
2024-07-26 20:19:11 +0800

    finally done with the getImageURL function gonna go to the other steps

--- main.py
+++ main.py
@@ -4,47 +4,68 @@ import sys
 import os
 import random
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 
-# def testtings():
-#     html_text = requests.get("https://unsplash.com").text
-#     soup = BeautifulSoup(html_text, 'lxml')
-#     picture_container = soup.find('div', class_="NHQ0m").find_all('div', )
-#     print(len(picture_container))
-    
+def testtings():
+    html_text = requests.get("https://unsplash.com/photos/a-camper-van-parked-in-a-field-with-the-sun-setting-in-the-background-0HPzoJ1sAwM").text
+    soup = BeautifulSoup(html_text, 'lxml')
+    container = soup.find('button', {"aria-label": "Zoom in on this image"}).find('div').find_all('img')
+    # print(len(container))
+    with open('output.html', 'w', encoding='utf-8') as file: 
+        file.write(str(container[1]))
+
+# FUNCTION TO GET IMAGE's URL 
 def getImagesURL():
-    html_text = requests.get("https://unsplash.com").text # Getting the website to scrape
-    soup = BeautifulSoup(html_text, 'lxml') # beautiful with soup 
-    picture_container = soup.find('div', class_="d95fI") # getting the main div as an object to the children
+    html_text = requests.get("https://unsplash.com/").text # Getting the website to scrape
+    soup = BeautifulSoup(html_text, "lxml") # Make a soup 
+
+    photo_containers = soup.find('div', {'data-test': 'masonry-grid-count-three'}) # get the main container to scrape
     
-    if not picture_container: # Checks if the main div from unsplash is available
-        return print("Picture container was not found (probably they changed the classname)")
+    if not photo_containers: # checks if it exists
+        return print('either the data-test attribute is replaced or something else happen idk')
     
-    anchor_tags = picture_container.find_all('a', class_="Prxeh") # getting all of the available links from the main div
-    links = [anchor['href'] for anchor in anchor_tags if 'href' in anchor.attrs] # containing all of the available links
+    photo_grids = photo_containers.find_all('div', style=lambda value: value and '--row-gutter:24px' in value)
     
-    if not links: # Checks if there are any links
-        return print("Link was nor found or this is just being a little stupid")
+    if not photo_grids: # checks if it exists
+        return print('either the style for --row-gutter:24px is replaced or something else happened idk')
     
-    list_amount = len(links) # Counting how many links there are available
-    random_image = random.randint(0, list_amount) # getting things randomized from 0 to the amount of available links
+    choose_random_grid = random.randint(0, 2) #choose 1 of 3 containers to have more random image
+    chosen_grid = photo_grids[choose_random_grid] # getting the chosen grid
     
-    print(random_image)
+    get_anchors = chosen_grid.find_all('a', {'itemprop': 'contentUrl'}) # Getting all of the available links from the chosen grid
+    if not get_anchors: # checks if it exists
+        return print("probably they changed the anchor's attribute name (previously was itemprop) or they changed the value of itemprop")
     
-    getImage(links[random_image - 1]) # Give it to other function to process we done here
+    links = [anchor['href'] for anchor in get_anchors if 'href' in anchor.attrs] # get their links individually
+    if not links: # checks if it exists
+        return print("Link was not found!")
     
+    links_count = len(links) # counting links (could've just put it directly inside the bottom var but for a better reading this is best... i think)
+    
+    choose_random_link = random.randint(0, links_count - 1) # getting random number 0 to whatever the links_count is
+    chosen_link = links[choose_random_link] # choosing the image by random
+    
+    print("grid yang dipilih: " + str(choose_random_grid + 1)) # EXPLANATION!!!!!!!!!
+    print("Link yang dipilih: " +  f"({str(choose_random_link)}) https://unsplash.com" + str(links[choose_random_link]) ) # EXPLANATION !!!!!!!!!!!!!!!!!!!!
+    
+    getImage(chosen_link) # getting ready to get the image by the chosen link
+
+# FUNCTION TO GET THE ACTUAL IMAGE THE PAINTING THE ARTFUL THE PORTRAITS 
 def getImage(link):
     html_text = requests.get("https://unsplash.com"+link).text # getting the image's url 
     soup = BeautifulSoup(html_text, 'lxml') # soupy
-    imageContainer = soup.find("div", class_="HcSeS") # get the main container to scrape (for image)
     
-    getImageSource = imageContainer.find("img", class_="ApbSI z1piP vkrMA")['srcset'] # after that get the image's online link so that we can use
-    getCaption = soup.find('h1', class_="vev3s").text
+    photo_container = soup.find('button', {"aria-label": "Zoom in on this image"}) # getting the main container for the iamge
+    if not photo_container:
+        return print('the container button was not found they probably changed the aria-label or somethinge lse... damn...')
     
-    print(getCaption)
+    find_image = photo_container.find('div').find_all('img') # first after we got to the child div we will be searching for all images to get (the parent is photo_contaienr btw)
+    
     
 
+# Send message
 def sendImages(): 
     base_url = os.getenv('baseurl')
     parameters = {
@@ -61,6 +82,6 @@ def sendImages():
     print(response)
 
 if __name__ == "__main__":
-    getImagesURL()
+    # getImagesURL()
     # sendImages()
-    # testtings()
+    testtings()
